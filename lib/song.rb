3aa57af4a09e19d8dcc4da2ffcc25e4dc4021364
Christopher Luna <chris@illusionlayervfx.com>
2019-06-19 04:11:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,11 +19,10 @@ class Song
 
   def artist_name
     if self.artist
-        self.artist.name
-      else
-        nil
-      end
-
+      self.artist.name
+    else
+      nil
+    end
   end
 
 end
